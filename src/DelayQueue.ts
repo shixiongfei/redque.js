b90d735bf17dfd18530a5e62a29338896b11a47a
shixiongfei <jenson.shixf@gmail.com>
2022-11-18 17:19:54 +0800

    Add delay queue consumer

--- src/DelayQueue.ts
+++ src/DelayQueue.ts
@@ -18,8 +18,7 @@ export class DelayQueue extends BaseRedis {
       const script = `
         redis.call('ZADD', KEYS[1], ARGV[1], ARGV[2])
         redis.call('HSET', KEYS[2], ARGV[2], ARGV[3])
-        return 1
-      `;
+        return 1`;
       scriptSha = await this.redis.scriptLoad(script);
       await this.redis.set(DelayQueue.PRODUCER_SCRIPT_SHA, scriptSha);
     }
@@ -27,6 +26,34 @@ export class DelayQueue extends BaseRedis {
     return await this.redis.evalSha(scriptSha, {
       keys: [this.name, `${this.name}:hash`],
       arguments: [`${score}`, ulid(), message],
+    });
+  }
+
+  async consumer(maxScore: number, count = 10) {
+    let scriptSha = await this.redis.get(DelayQueue.CONSUMER_SCRIPT_SHA);
+
+    if (!scriptSha) {
+      const script = `
+        local status, type = next(redis.call('TYPE', KEYS[1]))
+        if status ~= nil and status == 'ok' then
+          if type == 'zset' then
+              local list = redis.call('ZREVRANGEBYSCORE', KEYS[1], ARGV[1], ARGV[2], 'LIMIT', ARGV[3], ARGV[4])
+              if list ~= nil and #list > 0 then
+                  redis.call('ZREM', KEYS[1], unpack(list))
+                  local result = redis.call('HMGET', KEYS[2], unpack(list))
+                  redis.call('HDEL', KEYS[2], unpack(list))
+                  return result
+              end
+          end
+        end
+        return nil`;
+      scriptSha = await this.redis.scriptLoad(script);
+      await this.redis.set(DelayQueue.CONSUMER_SCRIPT_SHA, scriptSha);
+    }
+
+    return await this.redis.evalSha(scriptSha, {
+      keys: [this.name, `${this.name}:hash`],
+      arguments: [`${maxScore}`, "0", "0", `${count}`],
     });
   }
 }
